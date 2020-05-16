d95ac81993bf8ff861062a31d795299a6091386f
qin-nz <h-dy@outlook.com>
2020-05-16 15:02:24 +0800

    v6.3.15

---
+++ libctp.go
@@ -0,0 +1,7 @@
+package libctp
+
+/*
+#cgo linux LDFLAGS: -fPIC -L${SRCDIR}/v6.3.15_20190220_api_tradeapi_se_linux64 -Wl,-rpath,${SRCDIR}/v6.3.15_20190220_api_tradeapi_se_linux64 -lthostmduserapi_se -lthosttraderapi_se -lstdc++
+#cgo linux CPPFLAGS: -fPIC -I${SRCDIR}/v6.3.15_20190220_api_tradeapi_se_linux64
+*/
+import "C"
